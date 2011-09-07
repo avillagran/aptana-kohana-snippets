88de5a49b15779ccd33613463ce7c68f325d811d
Andrés Villagrán <andres@villagranquiroz.cl>
2011-09-07 12:46:51 -0300

    Routes, Get-Set views (kohana rails)

--- bundle.rb
+++ bundle.rb
@@ -5,10 +5,18 @@ bundle 'Kohana Snippets' do |bundle|
 	bundle.display_name = "Kohana"
 	
 	bundle.menu 'Kohana' do |main_menu|
-	  main_menu.menu 'Controllers' do |submenu|
-	    submenu.command 'controller'
-	    submenu.command 'Kohana Rails Action'
-    end		
+    
+    main_menu.command 'controller'		
 		main_menu.command 'model'
+		main_menu.menu 'Routes' do |s|
+		  s.command 'Route with directory'
+		  s.command 'Route'
+		end
+		
+		main_menu.menu 'Kohana Rails' do |submenu|
+		  submenu.command 'Kohana Rails Controller Action'
+      submenu.command 'Kohana Rails Controller Get - Set View' 
+		end
+		
 	end
 end
--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -14,20 +14,6 @@ class Controller_${1:ClassName} extends Controller_${2:Application} {
 }"
 end
 
-snippet 'Kohana Rails Action' do |s|
-  s.trigger ='action'
-  s.expansion = "
-function action_${1:name}()
-{
-  \\$view = \\$this->get_view();
-  
-  $0
-  
-  \\$this->set_view(\\$view);
-}
-"
-end
-
 snippet 'model' do |s|
   s.trigger = 'model'
   s.expansion = "<?php defined('SYSPATH') or die('No direct script access.');
@@ -37,6 +23,48 @@ snippet 'model' do |s|
 class Model_${1:ClassName} extends ${2:ORM} {
   $0
 }"
+end
+
+snippet 'Route with directory' do |s|
+  s.trigger = 'routed'
+  s.expansion = "Route::set('${1:name}', '${2:directory}/${3:controller}/${4:action}')
+  ->defaults(array(
+    'directory'  => '${2:directory}',
+    'controller' => '${3:controller}',
+    'action'     => '${4:action}'
+  ));"
+end
+
+snippet 'Route' do |s|
+  s.trigger = 'route'
+  s.expansion = "Route::set('${1:name}', '${2:controller}/${3:action}')
+  ->defaults(array(
+    'controller' => '${2:controller}',
+    'action'     => '${3:action}'
+  ));"
+end
+
+snippet 'Kohana Rails Controller Action' do |s|
+  s.trigger ='action'
+  s.expansion = "
+function action_${1:name}()
+{
+  \\$view = \\$this->get_view();
+
+  $0
+  
+  \\$this->set_view(\\$view);
+}
+"
+end
+
+snippet 'Kohana Rails Controller Get - Set View' do |s|
+  s.trigger = "getsetview"
+  s.expansion = "\\$view = \\$this->get_view();
+
+$0
+
+\\$this->set_view(\\$view);"
 end
 
 
